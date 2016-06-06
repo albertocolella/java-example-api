247be96b031dc86b2e2a4024e7bb13afa712d4ea
Alberto <kongoji@gmail.com>
2016-06-07 00:41:51 +0200

    First setup

---
+++ Readme.md
@@ -0,0 +1,3 @@
+= Java Example API =
+
+It uses [Java Rest Api](https://github.com/kongoji6/java-rest-api) as subproject
---
+++ app/src/main/java/com/albertocolella/example_api/App.java
@@ -0,0 +1,9 @@
+package com.albertocolella.example_api;
+
+public class App extends com.albertocolella.rest_bootstrap.App {
+
+	/*public static void main(String[] args) throws Exception {
+		System.out.println("HELLOWORLD");
+	}*/
+}
+
